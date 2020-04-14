b32012e8441197a679d22b0e180ae7dc8d1e6091
emileerei <reiche2@rpi.edu>
2020-04-14 08:46:41 -0700

    Able to load external json files, currently only using one

--- js/main.js
+++ js/main.js
@@ -1,17 +1,22 @@
 const paintingList = document.getElementById("paintingList");
 const searchResultString = document.getElementById("searchString");
 const searchBar = document.getElementById("searchBar");
-let paintingDatabase = [
-  { name: "Gaspar Rem", artist: "AACHEN, Hans von", timeline: "1601-1650", date: "1574-75", technique: "Oil on canvas", id: "1", palette: ["#aaa", "#bbb", "#ccc", "#ddd", "#eee"], img: "https://upload.wikimedia.org/wikipedia/commons/9/98/Hans_von_Aachen_-_Portrait_of_the_painter_Gaspar_Rem.jpg"},
-  { name: "Venus and Adonis", artist: "AACHEN, Hans von", timeline: "1601-1650", date: "1574-88", technique: "Oil on canvas", id: "2", palette: ["#aaa", "#bbb", "#ccc", "#ddd", "#eee"], img: "https://media.getty.edu/museum/images/web/enlarge/00103001.jpg"},
-  { name: "The Rose Garden", artist: "AAGAARD, Carl Frederik", timeline: "1851-1900", date: "1877", technique: "Oil on canvas", id: "22", palette: ["#aaa", "#bbb", "#ccc", "#ddd", "#eee"], img: "https://upload.wikimedia.org/wikipedia/commons/4/4f/The_Rose_Garden-Carl_Aagaard.jpg"},
-  { name: "The Archangel Michael", artist: "ABADIA, Juan de la", timeline: "1451-1500", date: "c. 1490", technique: "Wood", id: "24", palette: ["#aaa", "#bbb", "#ccc", "#ddd", "#eee"], img: "https://upload.wikimedia.org/wikipedia/commons/c/cf/Michael_abadia.jpg"},
-  { name: "Man with Parrot", artist: "ABBATE, Niccolò dell", timeline: "1501-1550", date: "1540s", technique: "Oil on canvas", id: "25", palette: ["#aaa", "#bbb", "#ccc", "#ddd", "#eee"], img: "https://upload.wikimedia.org/wikipedia/commons/0/03/NdellAbateViennaportrait.jpg"}
-  , { name: "Portrait of a Gentleman with a Falcon", artist: "ABBATE, Niccolò dell", timeline: "1501-1550", date: "1548-50", technique: "Oil on canvas", id: "27", palette: ["#aaa", "#bbb", "#ccc", "#ddd", "#eee"], img: "https://media.artgallery.nsw.gov.au/collection_images/1/167.1991%23%23S.jpg"}
-  , { name: "The Wounded Philoctetes", artist: "ABILDGAARD, Nicolai", timeline: "1751-1800", date: "1775", technique: "Oil on canvas", id: "43", palette: ["#aaa", "#bbb", "#ccc", "#ddd", "#eee"], img: "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3c/Den_s%C3%A5rede_Filoktet.jpg/250px-Den_s%C3%A5rede_Filoktet.jpg"}
-  , { name: "Fireworks in Naples", artist: "ACHENBACH, Oswald", timeline: "1851-1900", date: "1875", technique: "Oil on canvas", id: "47", palette: ["#aaa", "#bbb", "#ccc", "#ddd", "#eee"], img: "https://upload.wikimedia.org/wikipedia/commons/c/ce/Oswald_Achenbach_001.jpg"}
-  , { name: "Battle of Moscow on 7 September 1812", artist: "ADAM, Albrecht", timeline: "1801-1850", date: "1815-25", technique: "Oil and gouache", id: "48", palette: ["#aaa", "#bbb", "#ccc", "#ddd", "#eee"], img: "https://upload.wikimedia.org/wikipedia/commons/d/d4/Battle_of_Moscow_7_Septembr_1812_year.jpg"}
-  , { name: "Fish and Dead Game", artist: "ADRIAENSSEN, Alexander", timeline: "1601-1650", date: "1643", technique: "Oil on canvas", id: "64", palette: ["#aaa", "#bbb", "#ccc", "#ddd", "#eee"], img: "https://upload.wikimedia.org/wikipedia/commons/1/10/Alexander_Adriaenssen_-_Fish_and_Dead_Game_-_WGA0033.jpg"}];
+
+function loadJSON(callback) {
+  var xobj = new XMLHttpRequest();
+  xobj.overrideMimeType("application/json");
+  xobj.open("GET", "q.json", false);
+  xobj.onreadystatechange = function () {
+    if (xobj.readyState == 4 && xobj.status == "200") {
+      callback(xobj.responseText);
+    }
+  };
+  xobj.send(null);
+}
+
+var paintingDatabase;
+loadJSON(function (json) { paintingDatabase = JSON.parse(json); });
+console.log(paintingDatabase);
 
 searchBar.addEventListener("keyup", e => {
   const searchString = e.target.value.toLowerCase();
@@ -21,16 +26,16 @@ searchBar.addEventListener("keyup", e => {
   const parsedString = parseString(searchString);
   let fp = [];
   parsedString.forEach(element => {
-    if( element.startsWith("name:") ) {
+    if( element.startsWith("title:") ) {
       fp.push(paintingDatabase.filter(painting => {
         return (
-          painting.name.toLowerCase().includes(element.replace("name:", ""))
+          painting.title.toLowerCase().includes(element.replace("title:", ""))
         );
       }));
-    } else if( element.startsWith("artist:") ) {
+    } else if( element.startsWith("author:") ) {
       fp.push(paintingDatabase.filter(painting => {
         return (
-          painting.artist.toLowerCase().includes(element.replace("artist:", ""))
+          painting.author.toLowerCase().includes(element.replace("author:", ""))
         );
       }));
     } else if( element.startsWith("timeline:") ) {
@@ -45,12 +50,18 @@ searchBar.addEventListener("keyup", e => {
           painting.technique.toLowerCase().includes(element.replace("technique:", ""))
         );
       }));
+    } else if( element.startsWith("type:") ) {
+      fp.push(paintingDatabase.filter(painting => {
+        return (
+          painting.painting_type.toLowerCase().includes(element.replace("type:", ""))
+        );
+      }));
     } else {
       fp.push(paintingDatabase.filter(painting => {
         // possibly add timeline, technique, and date
         return (
-          painting.name.toLowerCase().includes(element) ||
-          painting.artist.toLowerCase().includes(element)
+          painting.title.toLowerCase().includes(element) ||
+          painting.author.toLowerCase().includes(element)
         );
       }));
     }
@@ -74,14 +85,14 @@ const displayPaintings = (paintings) => {
   const htmlString = paintings.map((painting) => {
     return `
       <li class="painting">   
-        <p><img class="paintingimg" src="${painting.img}"></img> 
-        <div class="info"><strong>${painting.name}</strong> <br /> ${painting.artist}</div></p>
+        <p> 
+        <div class="info"><strong>${painting.title}</strong> <br /> ${painting.author}</div></p>
         <div class="palette">
-          <div style="background-color:${painting.palette[0]}" class="box"></div>
-          <div style="background-color:${painting.palette[1]}" class="box"></div>
-          <div style="background-color:${painting.palette[2]}" class="box"></div>
-          <div style="background-color:${painting.palette[3]}" class="box"></div>
-          <div style="background-color:${painting.palette[4]}" class="box"></div>
+          <div style="background-color:${painting.palette1}" class="box"></div>
+          <div style="background-color:${painting.palette2}" class="box"></div>
+          <div style="background-color:${painting.palette3}" class="box"></div>
+          <div style="background-color:${painting.palette4}" class="box"></div>
+          <div style="background-color:${painting.palette5}" class="box"></div>
         </div>
         <input type="checkbox" id="checkbox" name="checkbox" value="compare">
       </li>
